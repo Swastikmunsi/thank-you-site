67aa3cf8c2e54dc4c779f82fa42779148b443f99
Swastikmunsi <swastik00111@gmail.com>
2025-11-11 22:32:02 +0530

    okkkgyu

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,22 @@ import "./globals.css";
 
 export const metadata = {
   title: "I Have Something to Say...",
-  description: "This isn’t just a website. It’s something truly special, built with love — just for you. Open it and feel it unfold.",
+  description:
+    "This isn’t just a website. It’s something truly special, built with love — just for you. Open it and feel it unfold.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`antialiased`}
-      >
+      <body className="antialiased">
+        {/* 🎵 Background music */}
+        <audio autoPlay loop>
+          <source
+            src="https://limewire.com/d/z6Pda#Tmk3nYBspM"
+            type="audio/mpeg"
+          />
+        </audio>
+
         {children}
       </body>
     </html>
